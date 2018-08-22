ab30f819d7d193b21271946b0bc2155110619ca5
Hubert Hadyński <hubert.hadynski@gmail.com>
2018-08-22 15:44:49 +0200

    created http server

---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function(request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/') {
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            response.write(data);
+            response.end();
+        });
+
+    } else {
+        response.setHeader("Content-Type", "image/jpeg; charset=utf-8");
+        fs.readFile('./sadcat.jpg', function(err, data) {
+            response.write(data);
+            response.end();
+        });
+
+    }
+});
+
+server.listen(8080);
